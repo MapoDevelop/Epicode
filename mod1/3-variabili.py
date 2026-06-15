256977aad19c913c4680da7176140caa8143ea13
MapoDesign <elisaresmini@gmail.com>
2026-06-15 09:48:53 +0200

    first python code

---
+++ mod1/3-variabili.py
@@ -0,0 +1,10 @@
+nome = "Elisa"
+print("Il mio nome è " + nome)
+
+eta = input("Qual'è la tua età? ")
+
+print("La tua età è " + str(int(eta) * 2))
+
+a = 5
+b = 10
+print("La somma di a (" + str(a) + ") e b (" + str(b) + ") è " + str(a + b))
---
+++ mod1/4-tipidato.py
@@ -0,0 +1,23 @@
+x=7
+y=3
+
+somma = x + y
+
+print("La somma di x (" + str(x) + ") e y (" + str(y) + ") è " + str(somma))
+
+prezzo = 19.99
+quantità = 3
+totale = prezzo * quantità
+print("Il totale è " + str(totale))
+
+numero = 10
+
+if numero > 5:
+    print("True")
+else:
+    print("False")
+
+a = 5
+b = 2.5
+
+print("La somma di a (" + str(a) + ") e b (" + str(b) + ") è " + str(a + b))
