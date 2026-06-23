a6950535c05ceed7866f58c84b339c4c90f6a4c1
MapoDesign <elisaresmini@gmail.com>
2026-06-23 15:34:51 +0200

    esercizio liste

---
+++ 01_python_basics/17-list.py
@@ -0,0 +1,11 @@
+numeri =  [1, 2, 3, 4, 5]
+print(" Lista di numeri: ", numeri)
+
+# Aggiungi un numero alla lista
+numeri.append(6)
+print(" Lista di numeri dopo l'aggiunta: ", numeri)
+
+# Sostituisci ilterzo numero della lista con un nuovo numero
+numeri[2] = 10
+
+print(" Lista di numeri dopo la sostituzione: ", numeri)
